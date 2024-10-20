c20120fad2d63e1d55c7fbd8943dd213f536dfed
Thanos-M11 <kalaitzisa@gmail.com>
2024-10-20 12:29:57 +0300

    update filters with material components

--- src/app/filters/filter.model.ts
+++ src/app/filters/filter.model.ts
@@ -1,8 +1,8 @@
 export interface Filter {
   serialNumber?: string;
   driverId?: number;
-  startDate?: Date;
-  endDate?: Date;
+  startDate?: Date | null;
+  endDate?: Date | null;
   isApproved?: string;
   plate?: string;
 }
--- src/app/filters/filters.component.ts
+++ src/app/filters/filters.component.ts
@@ -1,24 +1,38 @@
-import { Component, DestroyRef, OnInit } from '@angular/core';
 import {
-  AbstractControl,
-  FormControl,
-  FormGroup,
-  ReactiveFormsModule,
-  Validators,
-} from '@angular/forms';
+  ChangeDetectionStrategy,
+  Component,
+  DestroyRef,
+  OnInit,
+  ViewEncapsulation,
+} from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { Filter } from './filter.model';
 import { FilterService } from '../services/filter.service';
-import { formatDate } from '../helper/helper';
 import { DriversService } from '../services/driver.service';
 import { DriverState, Vehicle } from '../vehicles/vehicles.model';
 import { VehiclesService } from '../services/vehicle.service';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInput } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { provideNativeDateAdapter } from '@angular/material/core';
 
 @Component({
   selector: 'app-filters',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInput,
+    MatDatepickerModule,
+  ],
   templateUrl: './filters.component.html',
   styleUrl: './filters.component.css',
+  providers: [provideNativeDateAdapter()],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class FiltersComponent implements OnInit {
   approvedDropDown = [
@@ -39,30 +53,38 @@ export class FiltersComponent implements OnInit {
 
   form = new FormGroup({
     serialNumber: new FormControl(),
-    driver: new FormControl(),
-    startDate: new FormControl(),
-    endDate: new FormControl(),
-    isApproved: new FormControl(),
+    driver: new FormControl(''),
+    dateRange: new FormGroup({
+      start: new FormControl<Date | null>(null),
+      end: new FormControl<Date | null>(null),
+    }),
+    isApproved: new FormControl(''),
   });
 
   ngOnInit() {
     this.driversDropDown = this.driversService.getDrivers();
-    this.vehicleService.selectedVehicle$.subscribe({
+    const subscription = this.vehicleService.selectedVehicle$.subscribe({
       next: (vehicle) => (this.vehicleSelected = vehicle!),
     });
+
+    this.destroyRef.onDestroy(() => subscription.unsubscribe());
   }
 
   onSubmit() {
     const newFilter: Filter = {
       serialNumber: this.form.value.serialNumber,
-      driverId: +this.form.value.driver,
+      driverId: +this.form.value.driver!,
       startDate:
-        this.form.value.startDate && new Date(this.form.value.startDate),
-      endDate: this.form.value.endDate && new Date(this.form.value.endDate),
-      isApproved: this.form.value.isApproved,
+        this.form.value.dateRange?.start &&
+        new Date(this.form.value.dateRange.start),
+      endDate:
+        this.form.value.dateRange?.end &&
+        new Date(this.form.value.dateRange.end),
+      isApproved: this.form.value.isApproved!,
     };
 
     this.filterService.setFilter(newFilter);
+    console.log(this.form.controls.dateRange.controls);
   }
 
   onClearFilter() {
