2632087713958dacb7b78c5373472be9a73d22fe
Thanos-M11 <kalaitzisa@gmail.com>
2024-10-26 10:00:07 +0300

    refactored drivers

--- src/app/drivers/driver.service.ts
+++ src/app/drivers/driver.service.ts
@@ -1,23 +1,26 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 
-import { catchError, map, throwError } from 'rxjs';
+import { catchError, map, Observable, throwError } from 'rxjs';
 import { Driver } from '../drivers/drivers.model';
 
 @Injectable({ providedIn: 'root' })
 export class DriversService {
   private httpClient = inject(HttpClient);
+  private jsonUrl = 'data/drivers.json';
 
-  loadDrivers$() {
+  loadDrivers$(): Observable<Driver[]> {
     return this.fetchDrivers(
-      'http://localhost:3000/drivers',
+      this.jsonUrl,
       'Something went wrong fetching drivers'
     );
   }
 
-  private fetchDrivers(url: string, errorMessage: string) {
-    return this.httpClient.get<{ drivers: Driver[] }>(url).pipe(
-      map((resData) => resData.drivers),
+  private fetchDrivers(
+    url: string,
+    errorMessage: string
+  ): Observable<Driver[]> {
+    return this.httpClient.get<Driver[]>(url).pipe(
       catchError((error) => {
         console.log(error);
         return throwError(() => new Error(errorMessage));
