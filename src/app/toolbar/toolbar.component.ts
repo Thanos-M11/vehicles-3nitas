132a8c0831e6986cf65440f03db722984f14de03
Thanos-M11 <kalaitzisa@gmail.com>
2024-10-29 12:14:50 +0200

    minor updates of toolbar component

--- src/app/toolbar/toolbar.component.ts
+++ src/app/toolbar/toolbar.component.ts
@@ -1,25 +1,16 @@
-import { Component, DestroyRef, inject, OnInit } from '@angular/core';
-import { Vehicle } from '../vehicles/vehicles.model';
-import { VehiclesService } from '../vehicles/vehicle.service';
-import { Observable } from 'rxjs';
+import { Component, inject, OnInit } from '@angular/core';
 import { FilterService } from '../filters/filter.service';
-import { AsyncPipe } from '@angular/common';
 import { MatToolbar } from '@angular/material/toolbar';
 import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-toolbar',
   standalone: true,
-  imports: [AsyncPipe, MatToolbar],
+  imports: [MatToolbar],
   templateUrl: './toolbar.component.html',
   styleUrl: './toolbar.component.css',
 })
-export class ToolbarComponent implements OnInit {
-  selectedPlate!: string | null;
+export class ToolbarComponent {
   filterService = inject(FilterService);
   filter = toSignal(this.filterService.filter$);
-
-  constructor() {}
-
-  ngOnInit() {}
 }
